f4e71cac1b9aaac174f727a674ee7e8a5ec3250a
AngirisMiner <60354831+AngirisMiner@users.noreply.github.com>
2020-02-03 13:00:59 +0000

    Creation Of Folder System

---
+++ GhostsnGoblins/Assets/Ben/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour, Interface_Damage
+{
+
+    public Rigidbody2D rb;
+    public float speed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TakeDamage();
+    }
+
+    public void TakeDamage()
+    {
+
+        print ("hi");
+
+    }
+
+    void FixedUpdate()
+    {
+
+        if (Input.GetKey(KeyCode.D)) {
+
+            rb.AddForce(transform.right * speed, ForceMode2D.Impulse);
+
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+
+            rb.AddForce(-transform.right * speed, ForceMode2D.Impulse);
+
+        }
+
+    }
+}
