82ba878bdb31e7e0890800f85d99ba5b07cb2f01
ryan r <ryannowen@hotmail.co.uk>
2020-03-16 03:35:14 +0000

    CheckPoints + Killbox Player Addition
    Also deleted spawnsystem prefab and added script to game singleton

---
+++ GhostsnGoblins/Assets/Ryan/Scripts/CheckPoint.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            Singleton_Game.m_instance.SetCheckPoint(transform.position);
+        }
+    }
+}
