83463b9527292fa6053155b370ce05d0f7e6873a
ryan r <ryannowen@hotmail.co.uk>
2020-04-12 18:00:01 +0100

    Added volume control to options

---
+++ GhostsnGoblins/Assets/Ryan/Scripts/UI/optionsMenu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class optionsMenu : MonoBehaviour
+{
+    [SerializeField] private AudioMixer m_master = null;
+
+    public void ChangeGameVolume(float argVolume)
+    {
+        m_master.SetFloat("MasterVolume", argVolume);
+    }
+}
